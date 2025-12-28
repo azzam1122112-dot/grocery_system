0a723a956c8bc140f0341456b213668b4a4f6df6
xmansx1 <xmansx1122@gmail.com>
2025-12-28 22:48:14 +0300

    103

--- config/settings.py
+++ config/settings.py
@@ -117,10 +117,13 @@ USE_TZ = True
 
 
 
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Whitenoise static files storage
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
