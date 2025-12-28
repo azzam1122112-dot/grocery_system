d725a4e993c7041fff68748a654a0cebedfe706a
xmansx1 <xmansx1122@gmail.com>
2025-12-28 22:54:09 +0300

    104

--- config/settings.py
+++ config/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Whitenoise static files storage
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+WHITENOISE_MANIFEST_STRICT = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
